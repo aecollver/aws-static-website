4f4cb27239d805b7799672c95a1c95179fb3b888
Aaron Collver <aecollver@gmail.com>
2019-11-10 23:48:49 -0800

    Add content bucket and website distribution.

--- src/website.ts
+++ src/website.ts
@@ -1,9 +1,35 @@
-import { App, Construct, Stack, StackProps } from "@aws-cdk/core";
+import { CfnCloudFrontOriginAccessIdentity, CloudFrontWebDistribution } from "@aws-cdk/aws-cloudfront";
+import { CanonicalUserPrincipal, PolicyStatement } from "@aws-cdk/aws-iam";
+import { Bucket } from "@aws-cdk/aws-s3";
+import { App, Construct, Stack, StackProps, RemovalPolicy } from "@aws-cdk/core";
 import isValidDomain from "is-valid-domain";
 
 class WebsiteStack extends Stack {
   constructor(scope: Construct, name: string, props: StackProps) {
     super(scope, name, props);
+
+    // https://github.com/aws/aws-cdk/issues/941
+    const contentAccessIdentity = new CfnCloudFrontOriginAccessIdentity(this, "ContentAccessIdentity", {
+      cloudFrontOriginAccessIdentityConfig: {
+        comment: process.env.npm_config_domain_name
+      }
+    });
+
+    const content = new Bucket(this, "Content", {
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+    content.grantRead(new CanonicalUserPrincipal(contentAccessIdentity.getAtt("S3CanonicalUserId").toString()));
+
+    new CloudFrontWebDistribution(this, "Website", {
+      comment: process.env.npm_config_domain_name,
+      originConfigs: [{
+        behaviors: [{ isDefaultBehavior: true }],
+        s3OriginSource: {
+          s3BucketSource: content,
+          originAccessIdentityId: contentAccessIdentity.ref
+        }
+      }]
+    });
   }
 }
 
